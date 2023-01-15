0d3c4d8b1c7048031c0c2340813734515723bb15
Chris Rowles <christopher.rowles@outlook.com>
2023-01-15 16:15:25 +0000

    add basic auth middleware

---
+++ src/server/middleware/auth.ts
@@ -0,0 +1,34 @@
+import type { RequestHandler } from 'express';
+import { settings } from  '../config';
+
+function extractAuthHeader(header: string | undefined, identifier = 'Bearer'): string {
+  if (!header) {
+    throw new Error()
+  }
+
+  const parts = header.split(' ');
+
+  if (parts.length !== 2 || parts[0] !== identifier) {
+    throw new Error()
+  }
+
+  try {
+    return Buffer.from(parts[1], "base64").toString();
+  } catch (_) {
+    throw new Error()
+  }
+}
+
+export const verifyBasicAuth: RequestHandler = async(req, res, next) => {
+  try {
+    const decoded = extractAuthHeader(req.header('Authorization'));
+
+    if (decoded !== settings.app.secret) {
+      throw new Error();
+    }
+
+    next();
+  } catch (_) {
+    return res.sendStatus(401);
+  }
+}
--- src/server/routes/import.ts
+++ src/server/routes/import.ts
@@ -1,9 +1,11 @@
 import type { Router } from 'express-serve-static-core';
+import { verifyBasicAuth } from '../middleware/auth';
 import ImportController from '../controllers/ImportController';
 
+const middleware = [verifyBasicAuth];
 const controller = new ImportController;
 
 export const useImportRoutes = (router: Router) => {
-    router.get('/import/json', controller.json.bind(controller));
-    router.get('/import/mongo', controller.mongo.bind(controller));
+    router.get('/import/json', middleware, controller.json.bind(controller));
+    router.get('/import/mongo', middleware, controller.mongo.bind(controller));
 };
