0be69a30b0836f1051ac6ef5a750bb7570503051
Chris Rowles <c.rowles@sentrybay.com>
2023-03-11 16:02:31 +0000

    all

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,9 @@
 import type { Router } from 'express-serve-static-core';
-import { verifyBasicAuth } from '../middleware/auth';
 import AuthController from '../controllers/AuthController';
 
 const controller = new AuthController;
 
 export const useAuthRoutes = (router: Router) => {
-  router.post('/auth/login', [verifyBasicAuth], controller.login.bind(controller));
-  router.post('/auth/register', [verifyBasicAuth], controller.register.bind(controller));
+  router.post('/auth/login', controller.login.bind(controller));
+  router.post('/auth/register', controller.register.bind(controller));
 };
